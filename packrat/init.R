8270a73280f9407be337b43ebaa58a630ac7692b
drayllih-reverse <44709211+drayllih-reverse@users.noreply.github.com>
2019-03-28 19:45:15 +0000

    packrat bundling

--- packrat/init.R
+++ packrat/init.R
@@ -186,7 +186,7 @@ local({
     ## an 'installed from source' version
 
     ## -- InstallAgent -- ##
-    installAgent <- "InstallAgent: packrat 0.5.0"
+    installAgent <- "InstallAgent: packrat 0.5.0-6"
 
     ## -- InstallSource -- ##
     installSource <- "InstallSource: source"
